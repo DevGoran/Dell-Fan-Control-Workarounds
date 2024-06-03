79c365443f68191022aca9b453dba286d7c81c56
Goran <82758334+DevGoran@users.noreply.github.com>
2024-06-03 23:07:16 +0200

    Update setspeed.sh
    Added some more variables and adjusted the filter value in line 25 to 'Inlet Temp', as it seems this is only what's available besides TEMP (which would be the CPU temparatures). Script still not working as intended.

--- setspeed.sh
+++ setspeed.sh
@@ -10,7 +10,7 @@ IPMIPW=y0urPassw0rd
 IPMIEK=0000000000000000000000000000000000000000
 # Fan Speed / utilisation in percentage, for example 20 for 20% utilisation
 # Please note that each fan can have a different rpm and will not all be the same speed
-FANSPEED=20
+FANSPEED1=20
 FANSPEED2=30
 FANSPEED3=40
 FANSPEED4=55
@@ -22,10 +22,13 @@ MAXTEMP=60
 # This variable sends a IPMI command to get the temperature, and outputs it as two digits.
 # Do not edit unless you know what you do.
 # Side note, if you are running ipmitool on the system you are controlling, you don't need to specify -H,-U,-P - from the OS installed on the host, ipmitool is assumed permitted. You only need host/user/pass for remote access. 
-TEMP=$(ipmitool -I lanplus -H $IPMIHOST -U $IPMIUSER -P $IPMIPW -y $IPMIEK sdr type temperature |grep Ambient |grep degrees |grep -Po '\d{2}' | tail -1)
+TEMP=$(ipmitool -I lanplus -H $IPMIHOST -U $IPMIUSER -P $IPMIPW -y $IPMIEK sdr type temperature | grep 'Inlet Temp' | grep degrees | grep -Po '\d{2}' | tail -1)
 
 # Dont edit this, this converts decimal to hex
-SPEEDHEX=$( printf "%x" $FANSPEED )
+SPEEDHEX1=$( printf "%x" $FANSPEED1 )
+SPEEDHEX2=$( printf "%x" $FANSPEED2 )
+SPEEDHEX3=$( printf "%x" $FANSPEED3 )
+SPEEDHEX4=$( printf "%x" $FANSPEED4 )
 
 if [[ $TEMP > $MAXTEMP ]];
   then
@@ -39,5 +42,5 @@ if [[ $TEMP > $MAXTEMP ]];
     # This sets the fans to manual mode
     ipmitool -I lanplus -H $IPMIHOST -U $IPMIUSER -P $IPMIPW -y $IPMIEK raw 0x30 0x30 0x01 0x00
     # This is where we set the slower, quite speed
-    ipmitool -I lanplus -H $IPMIHOST -U $IPMIUSER -P $IPMIPW -y $IPMIEK raw 0x30 0x30 0x02 0xff 0x$SPEEDHEX
+    ipmitool -I lanplus -H $IPMIHOST -U $IPMIUSER -P $IPMIPW -y $IPMIEK raw 0x30 0x30 0x02 0xff 0x$SPEEDHEX1
 fi
