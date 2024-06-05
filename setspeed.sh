4064008532d25d803b56ce8a4a8a07beec2a5ee6
Goran <82758334+DevGoran@users.noreply.github.com>
2024-06-05 07:56:26 +0200

    Update setspeed.sh
    First try of actual implementation, not yet tested.

--- setspeed.sh
+++ setspeed.sh
@@ -8,39 +8,46 @@ IPMIUSER=yourIPMIuSer
 IPMIPW=y0urPassw0rd
 # Your IPMI Encryption Key (40x 0's are the default value). Value is set in iDrac settings.
 IPMIEK=0000000000000000000000000000000000000000
-# Fan Speed / utilisation in percentage, for example 20 for 20% utilisation
-# Please note that each fan can have a different rpm and will not all be the same speed
-FANSPEED1=20
-FANSPEED2=30
-FANSPEED3=40
-FANSPEED4=55
-# TEMPERATURE
-# Change this to the temperature in Celsius you are comfortable with.
-# If the temperature goes above the set degrees it will send raw IPMI command to enable dynamic fan control. With non-supported GPU's fan speed will be at minimum 75%.
-MAXTEMP=60
+# Fan Speeds / utilisation in percentage
+FANSPEED1=25
+FANSPEED2=35
+FANSPEED3=45
+# Temperature thresholds in Celsius
+TEMP1=35
+TEMP2=45
+TEMP3=55
 
-# This variable sends a IPMI command to get the temperature, and outputs it as two digits.
-# Do not edit unless you know what you do.
+# This variable sends an IPMI command to get the temperature and outputs it as two digits.
 # Side note, if you are running ipmitool on the system you are controlling, you don't need to specify -H,-U,-P - from the OS installed on the host, ipmitool is assumed permitted. You only need host/user/pass for remote access. 
 TEMP=$(ipmitool -I lanplus -H $IPMIHOST -U $IPMIUSER -P $IPMIPW -y $IPMIEK sdr type temperature | grep 'Inlet Temp' | grep degrees | grep -Po '\d{2}' | tail -1)
 
-# Dont edit this, this converts decimal to hex
+# Convert fan speeds to hex
 SPEEDHEX1=$( printf "%x" $FANSPEED1 )
 SPEEDHEX2=$( printf "%x" $FANSPEED2 )
 SPEEDHEX3=$( printf "%x" $FANSPEED3 )
-SPEEDHEX4=$( printf "%x" $FANSPEED4 )
 
-if [[ $TEMP > $MAXTEMP ]];
-  then
-    printf "Warning: Temperature is too high! Activating dynamic fan control! ($TEMP C)" | systemd-cat -t T630-IPMI-TEMP
-    echo "Warning: Temperature is too high! Activating dynamic fan control! ($TEMP C)"
-    # This sets the fans to auto mode, so the motherboard will set it to a speed that it will need do cool the server down
-    ipmitool -I lanplus -H $IPMIHOST -U $IPMIUSER -P $IPMIPW -y $IPMIEK raw 0x30 0x30 0x01 0x01
-  else
-    printf "Temperature is OK ($TEMP C)" | systemd-cat -t T630-IPMI-TEMP
-    printf "Activating manual fan speeds! (1560 RPM)"
-    # This sets the fans to manual mode
-    ipmitool -I lanplus -H $IPMIHOST -U $IPMIUSER -P $IPMIPW -y $IPMIEK raw 0x30 0x30 0x01 0x00
-    # This is where we set the slower, quite speed
-    ipmitool -I lanplus -H $IPMIHOST -U $IPMIUSER -P $IPMIPW -y $IPMIEK raw 0x30 0x30 0x02 0xff 0x$SPEEDHEX1
+# Check and set fan speed based on temperature
+if [[ $TEMP -ge $TEMP3 ]]; then
+  printf "Warning: Temperature is too high! Activating dynamic fan control! ($TEMP C)\n" | systemd-cat -t T630-IPMI-TEMP
+  echo "Warning: Temperature is too high! Activating dynamic fan control! ($TEMP C)"
+  # Set fans to auto mode
+  ipmitool -I lanplus -H $IPMIHOST -U $IPMIUSER -P $IPMIPW -y $IPMIEK raw 0x30 0x30 0x01 0x01
+elif [[ $TEMP -ge $TEMP2 ]]; then
+  printf "Temperature is high ($TEMP C), setting fan speed to $FANSPEED3%%\n" | systemd-cat -t T630-IPMI-TEMP
+  echo "Temperature is high ($TEMP C), setting fan speed to $FANSPEED3%%"
+  # Set fans to manual mode and apply speed 3
+  ipmitool -I lanplus -H $IPMIHOST -U $IPMIUSER -P $IPMIPW -y $IPMIEK raw 0x30 0x30 0x01 0x00
+  ipmitool -I lanplus -H $IPMIHOST -U $IPMIUSER -P $IPMIPW -y $IPMIEK raw 0x30 0x30 0x02 0xff 0x$SPEEDHEX3
+elif [[ $TEMP -ge $TEMP1 ]]; then
+  printf "Temperature is moderate ($TEMP C), setting fan speed to $FANSPEED2%%\n" | systemd-cat -t T630-IPMI-TEMP
+  echo "Temperature is moderate ($TEMP C), setting fan speed to $FANSPEED2%%"
+  # Set fans to manual mode and apply speed 2
+  ipmitool -I lanplus -H $IPMIHOST -U $IPMIUSER -P $IPMIPW -y $IPMIEK raw 0x30 0x30 0x01 0x00
+  ipmitool -I lanplus -H $IPMIHOST -U $IPMIUSER -P $IPMIPW -y $IPMIEK raw 0x30 0x30 0x02 0xff 0x$SPEEDHEX2
+else
+  printf "Temperature is low ($TEMP C), setting fan speed to $FANSPEED1%%\n" | systemd-cat -t T630-IPMI-TEMP
+  echo "Temperature is low ($TEMP C), setting fan speed to $FANSPEED1%%"
+  # Set fans to manual mode and apply speed 1
+  ipmitool -I lanplus -H $IPMIHOST -U $IPMIUSER -P $IPMIPW -y $IPMIEK raw 0x30 0x30 0x01 0x00
+  ipmitool -I lanplus -H $IPMIHOST -U $IPMIUSER -P $IPMIPW -y $IPMIEK raw 0x30 0x30 0x02 0xff 0x$SPEEDHEX1
 fi
